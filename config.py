fc8547fa89042f2c3267c5d8c66c922800b51e76
fallos1 <20691489@students.latrobe.edu.au>
2021-08-07 18:31:46 +1000

    add config.py for user variables

---
+++ config.py
@@ -0,0 +1,12 @@
+# Plex username. e.g. "username@email.com"
+USERNAME = None 
+# Plex password
+PASSWORD = None 
+# Plex server name. e.g. "My Library"
+SERVER_NAME = None 
+# Movie library name. e.g. "Movies"
+LIBRARY_NAME = None
+# Port to run app
+PORT = 8050
+# Plotly debug mode
+DEBUG = False
